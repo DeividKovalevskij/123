953c2c3d72829f389cf1fa9c7c67cb556f49746d
DeividKovalevskij <115566395+DeividKovalevskij@users.noreply.github.com>
2022-12-05 18:04:25 +0200

    Update Tablica.c

--- Laboratorium/lab2/Tablica.c
+++ Laboratorium/lab2/Tablica.c
@@ -2,79 +2,189 @@
 // Tablica
 // 2022-10-25
 
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
-int find(float item, float *arr);
+#define tab_size 100
 
-void insert(float item, int pos, float *arr);
+typedef struct Item {
+  float value;
+  struct Item *next;
+}
+it;
 
-int size(float *arr);
-
-float findMax(float *arr);
-
-float findMin(float *arr);
-
-void printTable(float *arr);
-
+int find(float value, it *head);
+int locate(float value, it *head);
+int retrieve(it *item, it *head);
+void insert(it **head, float value, int pos); //200OK
+void remove_by_index(it **head,int pos); //200OK
+int size(it *head); //200OK
+it findMax(it *head)
+it findMin(it *head);
+void printList(it *head); //200OK
+int isEmpty(it *head); //200OK
+void next(it **item,float value); //200OK
+void previous(it **head, float value); //200OK
+void makeNull(it *head); //200OK
 void main(void) {
-  float arr[100] = {4.1,5.1,9.5,8.7,7.5,4.0};
-  // realizacja
-
-  int pozycja = find(4.1, arr);
-  printf("Pozycja: %d\n", pozycja);
-  float max = findMax(arr);
-  printf("Maksimum: %f\n", max);
-  float min = findMin(arr);
-  printf("Minimum: %f\n", min);
-  printTable(arr);
+  it *head;
+  head = (it *)malloc(sizeof(it));
+  head=NULL;
   
+  srand(time(NULL));
+  for (int i = 0; i < 3; i++) {
+    next(&head,rand()%100);
+    previous(&head,rand()%100);
+    insert(&head,rand()%100,rand()%size(head));
+  }
   return;
 }
 
-// Funkcji
+//Funkcji
 
-int find(float item, float *arr){
-  for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++){
-    if(item == arr[i]){
-      return i;
+void remove_by_index(it **head,int pos){
+  it *current = *head;
+  it *tmp;
+  int i=0;
+  while (current->next != NULL && i<pos-1) {
+    current=current->next;
+    i++;
+  }
+  tmp = current->next;
+  current->next = tmp->next;
+  free(tmp);
+}
+
+void insert(it **head, float value, int pos){
+  if(pos==0){
+    next(head, value);
+  }
+  else {
+    if(pos==size(*head)) previous(head, value);
+    else {
+      it *current=*head;
+      it *tmp;
+      
+      int i=0;
+      while (current->next != NULL && i<pos-1) {
+        current = current->next;
+        i++;
+      }
+      tmp=current->next;
+      current->next=(it *)malloc(sizeof(it));
+      current->next->value=value;
+      current->next->next=tmp;
     }
   }
-  return -1;
 }
 
-void insert(float item, int pos, float *arr){
-  arr[pos] = item;
+int isEmpty(it *head){
+  if(head==NULL){
+    return 1;
+  }
+  return 0;
 }
 
-int size(float *arr){
-  int i = (sizeof(arr)/sizeof(arr[0]));
-  printf("\n%d",i);
-  return i;
+void makeNull(it *head){
+  it *ptr1,*ptr2;
+  if (!head->next) return;
+  ptr1 = head->next;
+  while (ptr1) {
+    ptr2 = ptr1;
+    ptr1 = ptr1->next;
+    free(ptr2);
+  }
+  head->next = 0;
 }
 
-float findMax(float *arr){
-  float max = arr[0];
-  for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++){
-    if(max < arr[i]){
-      max = arr[i];
+void printList(it *head){
+  printf("\n");
+  if(head==NULL) printf("List is empty");
+  else {
+    it *current=head;
+    do {
+      printf("%i", current->value);
+      printf("\n");
+      urrent = current->next;
     }
+    while (current != NULL);
+  }
+}
+
+int size(it *head){
+  int counter=0;
+  if(head==NULL) return counter;
+  else {
+    it *current=head;
+    do {
+      counter++;
+      current = current->next;
+    }
+    while (current != NULL);
+  }
+  return counter;
+}
+
+void next(it **head,float value){
+  it *current;
+  current=(it *)malloc(sizeof(it));
+  current->value=value;
+  current->next=(*head);
+  *head=current;
+}
+
+int find(float value, it *head) {
+  it *current = head;
+  while (current != NULL) {
+    if (current->value == value) {
+      prinf("ta wartosc jest w liscie")
+        return 1;
+    }
+    current = current->next;
+  }
+  prinf("tei wartosc niema w liscie")
+  }
+return 0
+}
+
+int size(it *head){
+  int counter=0;
+  if(head==NULL) return counter;
+  else {
+    it *current=head;
+    do {
+      counter++;
+      current = current->next;
+    }
+    while (current != NULL);
+  }
+  return counter;
+}
+
+int findMax(it **head) {
+  it *current;
+  current = *head;
+  int max = current->value;
+  while (current != NULL) {
+    if (current->value > max) {
+      max = current->value;
+    }
+    current = current->next;
   }
   return max;
 }
 
-float findMin(float *arr){
-  float min = arr[0];
-  for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++){
-    if(min > arr[i]){
-      min = arr[i];
+int findMin(it **head) {
+  it *current;
+  current = *head;
+  int min = current->value;
+  while (current != NULL) {
+    if (current->value < max) {
+      min = current->value;
     }
+    current = current->next;
   }
   return min;
-}
-
-void printTable(float *arr){
-  for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++){
-    printf(" %.2f ", arr[i]);
-  }
 }
