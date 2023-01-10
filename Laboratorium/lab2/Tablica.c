4d83604cd181a7661cedd4d37eaffb44aa27d0a0
DeividKovalevskij <115566395+DeividKovalevskij@users.noreply.github.com>
2023-01-10 23:37:07 +0200

    Update Tablica.c

--- Laboratorium/lab2/Tablica.c
+++ Laboratorium/lab2/Tablica.c
@@ -7,184 +7,106 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define tab_size 100
+#define arr_size 100
 
-typedef struct Item {
-  float value;
-  struct Item *next;
-}
-it;
+int find(int item, int *arr);
 
-int find(float value, it *head);
-int locate(float value, it *head);
-int retrieve(it *item, it *head);
-void insert(it **head, float value, int pos); //200OK
-void remove_by_index(it **head,int pos); //200OK
-int size(it *head); //200OK
-it findMax(it *head)
-it findMin(it *head);
-void printList(it *head); //200OK
-int isEmpty(it *head); //200OK
-void next(it **item,float value); //200OK
-void previous(it **head, float value); //200OK
-void makeNull(it *head); //200OK
+void insert(int item, int pos, int *arr);
+
+void remove2(int pos, int *arr);
+
+int size();
+
+int findMax(int *arr);
+
+int findMin(int *arr);
+
+void printTable(int *arr);
+
 void main(void) {
-  it *head;
-  head = (it *)malloc(sizeof(it));
-  head=NULL;
-  
-  srand(time(NULL));
-  for (int i = 0; i < 3; i++) {
-    next(&head,rand()%100);
-    previous(&head,rand()%100);
-    insert(&head,rand()%100,rand()%size(head));
+  int arr[arr_size];
+  // realizacja
+
+  srand(time(NULL)); // use current time as seed for random generator
+  for (int i = 0; i < size(); i++) {
+    arr[i] = rand() % 100;
+    printf("%d ", arr[i]);
   }
   return;
 }
 
-//Funkcji
+// Funkcii
 
-void remove_by_index(it **head,int pos){
-  it *current = *head;
-  it *tmp;
-  int i=0;
-  while (current->next != NULL && i<pos-1) {
-    current=current->next;
-    i++;
-  }
-  tmp = current->next;
-  current->next = tmp->next;
-  free(tmp);
-}
+int size() {
+    return arr_size;
+ }
 
-void insert(it **head, float value, int pos){
-  if(pos==0){
-    next(head, value);
-  }
-  else {
-    if(pos==size(*head)) previous(head, value);
-    else {
-      it *current=*head;
-      it *tmp;
-      
-      int i=0;
-      while (current->next != NULL && i<pos-1) {
-        current = current->next;
-        i++;
+int find(int item, int *arr) {
+  printf("%d", size());
+  if (size(arr) > 0) {
+    for (int i = 0; i < size(arr); i++) {
+      printf("%d\n", arr[i]);
+      if (arr[i] == item) {
+        return 1;
       }
-      tmp=current->next;
-      current->next=(it *)malloc(sizeof(it));
-      current->next->value=value;
-      current->next->next=tmp;
     }
-  }
-}
-
-int isEmpty(it *head){
-  if(head==NULL){
-    return 1;
   }
   return 0;
 }
 
-void makeNull(it *head){
-  it *ptr1,*ptr2;
-  if (!head->next) return;
-  ptr1 = head->next;
-  while (ptr1) {
-    ptr2 = ptr1;
-    ptr1 = ptr1->next;
-    free(ptr2);
-  }
-  head->next = 0;
-}
+void insert(int item, int pos, int *arr) { 
+    int tmp_arr[size() + 1];
+    int j=0;
+    for (int i = 0; i < size()+1; i++) {
+    if (i != pos) {
+      tmp_arr[i] = arr[j];
+      j++;
+    } else {
 
-void printList(it *head){
-  printf("\n");
-  if(head==NULL) printf("List is empty");
-  else {
-    it *current=head;
-    do {
-      printf("%i", current->value);
-      printf("\n");
-      urrent = current->next;
+      tmp_arr[i] = item;
+      
     }
-    while (current != NULL);
   }
-}
+  *arr = *tmp_arr; 
+  }
 
-int size(it *head){
-  int counter=0;
-  if(head==NULL) return counter;
-  else {
-    it *current=head;
-    do {
-      counter++;
-      current = current->next;
+void remove2(int pos, int *arr) {
+  int tmp_arr[size() - 1];
+  for (int i = 1; i < size(); i++) {
+    if (i != pos) {
+      tmp_arr[i] = arr[i];
+    } else {
+      i++;
+      tmp_arr[i--] = arr[i];
     }
-    while (current != NULL);
   }
-  return counter;
+  *arr = *tmp_arr;
 }
 
-void next(it **head,float value){
-  it *current;
-  current=(it *)malloc(sizeof(it));
-  current->value=value;
-  current->next=(*head);
-  *head=current;
-}
-
-int find(float value, it *head) {
-  it *current = head;
-  while (current != NULL) {
-    if (current->value == value) {
-      prinf("ta wartosc jest w liscie")
-        return 1;
+int findMax(int *arr) {
+  int max = arr[0];
+  for (int i = 0; i < size(arr); i++) {
+    if (max < arr[i]) {
+      max = arr[i];
     }
-    current = current->next;
-  }
-  prinf("tei wartosc niema w liscie")
-  }
-return 0
-}
-
-int size(it *head){
-  int counter=0;
-  if(head==NULL) return counter;
-  else {
-    it *current=head;
-    do {
-      counter++;
-      current = current->next;
-    }
-    while (current != NULL);
-  }
-  return counter;
-}
-
-int findMax(it **head) {
-  it *current;
-  current = *head;
-  int max = current->value;
-  while (current != NULL) {
-    if (current->value > max) {
-      max = current->value;
-    }
-    current = current->next;
   }
   return max;
 }
 
-int findMin(it **head) {
-  it *current;
-  current = *head;
-  int min = current->value;
-  while (current != NULL) {
-    if (current->value < max) {
-      min = current->value;
+int findMin(int *arr) {
+  int min = arr[0];
+  for (int i = 0; i < size(); i++) {
+    if (min > arr[i]) {
+      min = arr[i];
     }
-    current = current->next;
   }
   return min;
+}
+
+void printTable(int *arr) {
+  printf("\nPrint array \n------ \n");
+  for (int i = 0; i < size(); i++) {
+    printf(" %d \n", arr[i]);
+  }
+  printf("------\n");
 }
