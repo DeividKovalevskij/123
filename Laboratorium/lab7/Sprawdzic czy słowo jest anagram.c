d12e8a4dd14aa510b228e13ba1da540ea1b20eb0
DeividKovalevskij <115566395+DeividKovalevskij@users.noreply.github.com>
2023-02-01 15:18:12 +0200

    Create Sprawdzic czy słowo jest anagram.c

---
+++ Laboratorium/lab7/Sprawdzic czy słowo jest anagram.c
@@ -0,0 +1,47 @@
+//Devid Kovalevskij. Student
+
+#include <stdio.h>
+#include <string.h>
+#define MAX_WORD_LENGTH 50
+
+int is_anagram(char a[], char b[]) { 
+ 
+  if (strlen(a) != strlen(b)) {
+    return 0;
+  }
+
+  int i = 0;
+  int freq_a[26] = {0};
+  int freq_b[26] = {0};
+  
+  for (i = 0; a[i] && b[i]; i++) {
+    freq_a[a[i] - 97]++;
+    freq_b[b[i] - 97]++;
+  }
+  
+  for (i = 0; i < 26; i++) {
+    if (freq_a[i] != freq_b[i]) {
+      return 0;
+    }
+  }
+
+  return 1;
+}
+
+int main(void) {
+  char a[MAX_WORD_LENGTH], b[MAX_WORD_LENGTH];
+
+  printf("Wprowadź pierwszy ciąg : ");
+  scanf("%s", a); 
+
+  printf("Wprowadź drugi ciąg: ");
+  scanf("%s", b); 
+  
+  if (is_anagram(a, b) == 1) {
+    printf("Dwie struny są dla siebie anagramami");
+  } else {
+    printf("Dwie struny nie są dla siebie anagramami");
+  }
+
+  return 0;
+}
